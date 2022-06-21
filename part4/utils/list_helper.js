a2e65f81971043cbaeb1c726d13f164007806cab
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-21 15:00:11 +0530

    Exercise 4.4: helper functions and unit tests, Step-2
    Created tests for totalLikes

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
-const dummy = (_blogs) => {
-  return 1
+const dummy = (_blogs) => 1
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((prev, curr) => prev + curr.likes, 0)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
