b8a24b4b727752634a3424c1ea0d16497055d98b
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-21 15:23:57 +0530

    Exercise 4.5*: helper functions and unit tests, Step-3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,24 @@ const totalLikes = (blogs) => {
     : blogs.reduce((prev, curr) => prev + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+  if (blogs.length === 0) {
+    return {}
+  }
+
+  const max = Math.max(...blogs.map(blog => blog.likes))
+  const fav = blogs.find(blog => blog.likes === max)
+
+  return {
+    title: fav.title,
+    author: fav.author,
+    likes: fav.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
