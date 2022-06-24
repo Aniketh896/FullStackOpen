2c10fdf32677ca68cb95fd43f7ebb0d316ef8fcc
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-24 23:04:31 +0530

    Exercise 5.2: bloglist frontend, Step-2
    Added LocalStorage and Logout

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -16,17 +16,34 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {    
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')    
+    if (loggedUserJSON) {      
+      const user = JSON.parse(loggedUserJSON)      
+      setUser(user)      
+      //blogService.setToken(user.token)    
+    }  
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(        
+        'loggedBlogappUser', JSON.stringify(user)      
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (err) {
       console.error(err.message)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -53,7 +70,7 @@ const App = () => {
   const blogData = user => (
     <div>
       <h1>blogs</h1>
-      {user.name} logged in 
+      {user.name} logged in <input type='button' value='logout' onClick={handleLogout}/>
       <p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
